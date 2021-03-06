aa097c362e35357706f538b5098fc40067b74f67
Hunter-Kam <hone648@gmail.com>
2021-03-06 11:27:57 -0800

    added message when collection is empty

--- logic.js
+++ logic.js
@@ -13,10 +13,15 @@ const createMathProblems = (index1Max, index2Max) => {
 };
 
 const generateOneMathProblem = () => {
-  index = Math.floor(Math.random() * mathProblems.length);
-  const generatedProblem = mathProblems[index];
-  deleteOneMathProblem(index);
-  return generatedProblem;
+  if (mathProblems.length > 0) {
+    index = Math.floor(Math.random() * mathProblems.length);
+    const generatedProblem = mathProblems[index];
+    deleteOneMathProblem(index);
+    console.log(index);
+    return generatedProblem;
+  } else {
+    return "You have swallowed all of the math gravy in this collection.";
+  }
 };
 const deleteOneMathProblem = (index) => {
   mathProblems.splice(index, 1);
