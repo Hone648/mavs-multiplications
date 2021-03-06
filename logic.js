bd935ba639b6e5a128b8af29fe82e402d46df7a5
Hunter-Kam <hone648@gmail.com>
2021-03-06 14:12:29 -0800

    removed unnecessary function

--- logic.js
+++ logic.js
@@ -9,20 +9,4 @@ const createMathProblems = () => {
   return problems;
 };
 
-const generateOneMathProblem = () => {
-  if (mathProblems.length > 0) {
-    index = Math.floor(Math.random() * mathProblems.length);
-    const generatedProblem = mathProblems[index];
-    deleteOneMathProblem(index);
-    console.log(index);
-    return generatedProblem;
-  } else {
-    return "You have swallowed all of the math gravy in this collection.";
-  }
-};
-const deleteOneMathProblem = (index) => {
-  mathProblems.splice(index, 1);
-};
-
 const mathProblems = createMathProblems();
-console.log(mathProblems);
